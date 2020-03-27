7c0c9db7a07ba4698dcb17d7487239a4552b4b7f
Naruto-sys <gavriil.belov@yandex.ru>
2020-03-27 14:27:20 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from flask_login import LoginManager, login_user, logout_user, login_required
 from Forms.job_form import AddJobForm
 from Forms.login_form import LoginForm
@@ -6,6 +6,7 @@ from Forms.register_form import RegisterForm
 from data import db_session
 from data.jobs import Jobs
 from data.users import User
+from flask_login import current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -43,6 +44,7 @@ def login():
 
 
 @app.route('/addjob', methods=['GET', 'POST'])
+@login_required
 def add_job():
     form = AddJobForm()
     if form.validate_on_submit():
@@ -89,6 +91,48 @@ def reqister():
         session.commit()
         return redirect('')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/сhange_job/<int:job_id>', methods=['GET', 'POST'])
+@login_required
+def сhange_job(job_id):
+    form = AddJobForm()
+    form.submit.label.text = 'Изменить данные'
+    if request.method == "GET":
+        session = db_session.create_session()
+        job = session.query(Jobs).filter(Jobs.id == job_id).first()
+        form.job.data = job.job
+        form.work_size.data = job.work_size
+        form.collaborators.data = job.collaborators
+        form.team_leader.data = job.team_leader
+        form.is_finished.data = job.is_finished
+
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        job = session.query(Jobs).filter(Jobs.id == job_id).first()
+        if current_user.id == 1 or job.user.id == current_user.id and job:
+            job.job = form.job.data
+            job.work_size = form.work_size.data
+            job.collaborators = form.collaborators.data
+            job.is_finished = form.is_finished.data
+            job.team_leader = form.team_leader.data
+            session.add(job)
+            session.commit()
+            return redirect('')
+        else:
+            return render_template('add_job.html', title='Редактирование работы', form=form, message="У Вас нет на это прав!")
+    return render_template('add_job.html', title='Редактирование работы', form=form)
+
+
+@app.route('/delete_job/<int:job_id>', methods=['GET', 'POST'])
+@login_required
+def delete_job(job_id):
+    session = db_session.create_session()
+    job = session.query(Jobs).filter(Jobs.id == job_id).first()
+    if current_user.id == 1 or job.user.id == current_user.id:
+        session.delete(job)
+        session.commit()
+    return redirect('')
 
 
 @app.route('/')
